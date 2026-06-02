09eae2ac816e86c0997fde89a536abcebc04b295
Your Name <your.email@example.com>
2026-06-02 06:46:25 +0530

    fix: increase express body parser limit to support base64 images

--- server.ts
+++ server.ts
@@ -37,7 +37,8 @@ app.use(helmet({
   crossOriginEmbedderPolicy: false
 }));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Setup rate-limiting to defend API routes against spamming and empty submissions
 const apiRateLimiter = rateLimit({
